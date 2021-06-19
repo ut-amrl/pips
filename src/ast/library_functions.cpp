49093900f4e5e513e2e7f6dd9175ce336cbf0b44
jarrettHoltz <random.jmh@gmail.com>
2021-06-19 18:01:54 -0500

    Updating test infra

--- src/ast/library_functions.cpp
+++ src/ast/library_functions.cpp
@@ -382,8 +382,8 @@ ast_ptr StraightFreePathLength(ast_ptr v,
   //TODO(jaholtz) need to set these to sane defaults (copy from sim)
   float kRobotLength = 1.0;
   float kRearAxleOffset = 0.0;
-  float kObstacleMargin = 0.5;
-  float kRobotWidth = 0.86;
+  float kObstacleMargin = 0.2;
+  float kRobotWidth = 0.44;
   const bool cobot = false;
   if (cobot) {
     kRobotLength = 0.4;
